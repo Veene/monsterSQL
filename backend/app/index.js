90b0c02d4c2701cbac301138b5070c5dcc29de2f
John Pawlak <johnpawlak@gmail.com>
2019-01-24 20:41:30 -0500

    refactor and add express, add get methods

---
+++ backend/app/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const GenerationEngine = require('./generation/engine.js')
+const dragonRouter = require('./api/dragon.js')
+const generationRouter = require('./api/generation.js')
+
+const app = express()
+const engine = new GenerationEngine();
+
+app.locals.engine = engine;
+app.use('/dragon', dragonRouter)
+app.use('/generation', generationRouter)
+
+
+engine.start();
+app.get('/', (req, res) => {
+  res.send('Hello John');
+})
+//app.get( /dragon/new ) => gets new dragon in json format moved to api folder and is using ROUTER from express
+
+module.exports = app;
